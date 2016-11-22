0ec7efc99c1aca5d9195149571e4b2bf6098ec06
Adrien Vergé <adrienverge@gmail.com>
2016-11-22 11:36:41 +0100

    feat(dist): Distribute package

---
+++ setup.py
@@ -0,0 +1,17 @@
+# Copyright 2016 Adrien Vergé
+# All rights reserved
+
+from distutils.core import setup
+
+
+setup(
+    name='coucharchive',
+    version='1.0.0',
+    author='Adrien Vergé',
+    url='https://github.com/tolteck/coucharchive',
+
+    scripts=['coucharchive'],
+    requires=[
+        'CouchDB',
+    ],
+)
