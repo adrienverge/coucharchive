9be42e795b45477f5bac3c46864c45944773f69f
Adrien Vergé <adrienverge@gmail.com>
2017-10-06 14:42:01 +0200

    chore(version): coucharchive version 1.2.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='coucharchive',
-    version='1.1.0',
+    version='1.2.0',
     author='Adrien Vergé',
     url='https://github.com/adrienverge/coucharchive',
     license='MIT',
